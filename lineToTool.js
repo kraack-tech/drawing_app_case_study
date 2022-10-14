c84339bccf1d4d34095cd5267a88cc65939c61ab
kraack-tech <kriskraack@gmail.com>
2022-10-14 13:12:01 +0200

    fix code

--- lineToTool.js
+++ lineToTool.js
@@ -1,21 +1,25 @@
-function LineToTool(){
-	this.icon = "assets/lineTo.jpg";
-	this.name = "LineTo";
-
+//create a constructor function that adds a new tool which can create lines. (create function with image "assets/lineTo.jpg" as the icon. Set name to "LineTo" and create variables and assign dummy values.) 
+function LineToTool(){		
+	this.icon = "assets/lineTo.jpg";		
+	this.name = "LineTo";		
 	var startMouseX = -1;
 	var startMouseY = -1;
 	var drawing = false;
 
+//create method that draws line  
 	this.draw = function(){
-
+		
+		//conditionally on if mouse is pressed(left or right) and movement of mouse.
 		if(mouseIsPressed){
 			if(startMouseX == -1){
 				startMouseX = mouseX;
 				startMouseY = mouseY;
 				drawing = true;
+				//store pixel data into array (copy of pixels in memory)
 				loadPixels();
 			}
-
+			
+			//update/hide the canvas with stored pixels above.
 			else{
 				updatePixels();
 				line(startMouseX, startMouseY, mouseX, mouseY);
@@ -23,6 +27,7 @@ function LineToTool(){
 
 		}
 
+		//store and reset variables
 		else if(drawing){
 			drawing = false;
 			startMouseX = -1;
